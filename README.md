915374293829e57bbf7499a65a4e96f1f1e47038
aarush-narang <aarushnarang@gmail.com>
2022-06-22 18:34:30 -0700

    converted breaker into a class, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# password-breaker
- 
+# Wordle Clone
+
+Created using Python, this script will guess passwords either in plaintext or hashed if you know the algorithm.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the following dependencies
+
+- numpy
+
+Then, run
+
+```bash
+python path_to_script
+```
+
+## How to Use
+
+The script will prompt you for a hash method (such as plain, SHA256, SHA512, etc.) and a string that was hashed with that method. Then, it will brute force all possible passwords for that hash and return the first one that matches.
--- breaker.py
+++ breaker.py
@@ -3,43 +3,58 @@ import itertools
 import time
 import numpy as np
 
-def getSHA256(string: str):
-    return hashlib.sha256(str(string).encode('utf-8')).hexdigest()
-     
-def breakHash(actualHash):
-    chars = 'abcdefghijklmnopqrstuvwxyz' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + '1234567890' + '`-=~_+!@#$%^&*()[]\{\}\\|;\':",.<>/?'
-    attempts = 0
-    length = 1
-    while True:
-        for guess in itertools.product(chars, repeat=length):
-            attempts += 1
-            guessPlain = ''.join(guess)
-            guessHash = getSHA256(''.join(guess))
-            if guessHash == actualHash:
-                return f'password is {guessPlain}. found in {attempts} guesses.'
-        length += 1
+class Breaker:
+    __hashMethods = {
+        'plain': lambda x: x,
+        'md5': hashlib.md5,
+        'sha1': hashlib.sha1,
+        'sha224': hashlib.sha224,
+        'sha256': hashlib.sha256,
+        'sha384': hashlib.sha384,
+        'sha512': hashlib.sha512
+    }
+    def __init__(self):
+        self.method = 'sha256'
 
-def breakUnHashed(string):
-    chars = 'abcdefghijklmnopqrstuvwxyz' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + '1234567890' + '`-=~_+!@#$%^&*()[]\{\}\\|;\':",.<>/?'
-    attempts = 0
-    length = 1
-    while True:
-        for guess in itertools.product(chars, repeat=length):
-            attempts += 1
-            guessPlain = ''.join(guess)
-            if guessPlain == string:
-                return f'password is {guessPlain}. found in {attempts} guesses.'
-        length += 1
+    def __getHash(self, string: str):
+        if self.method != 'plain':
+            return self.__hashMethods[self.method](string).hexdigest()
+        else:
+            return string
 
+    def setHashMethod(self, method):
+        if method in self.__hashMethods:
+            self.method = method
+            return self
+        else:
+            raise ValueError('Invalid hash method')
+
+    def breakHash(self, hashedString):
+        chars = 'abcdefghijklmnopqrstuvwxyz' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + '1234567890' + '`-=~_+!@#$%^&*()[]\{\}\\|;\':",.<>/?'
+        attempts = 0
+        length = 1
+        while True:
+            for guess in itertools.product(chars, repeat=length):
+                attempts += 1
+                guessPlain = ''.join(guess)
+                guessHash = self.__getHash(''.join(guess))
+                if guessHash == hashedString:
+                    return f'password is {guessPlain}. found in {attempts} guesses.'
+            length += 1
 
 if __name__ == '__main__':
+
+    breaker = Breaker()
+
+    method = input('Enter hash method: ')
+    breaker.setHashMethod(method)
+
+    string = input('Enter string to break (hashed with the method you entered previously): ')
+
     startTime = time.time()
-
-    # actualHash = getSHA256('1234')
-    # print(breakHash(actualHash))
-
-    string = 'abcdef'
-    print(breakUnHashed(string))
-
+    res = breaker.breakHash(string)
     endTime = time.time()
+    
+    print(res)
+    
     print(f'{np.round((endTime - startTime), 3)} sec')
